e4aceb825848a8acb03a1db7f56f77b99ad1279e
hugodandois <hugo.dandois@student.vinci.be>
2024-10-17 22:51:57 +0200

    ex2.3

---
+++ tutorials/collections/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+
+const Header = (props: HeaderProps) => {
+    return (
+      <header>
+        <h1 className="animate__animated animate__bounce">{props.title}</h1>
+        <h4>Version: {props.version}</h4>
+      </header>
+    );
+};
+
+interface HeaderProps {
+    title: string;
+    version: number;
+}
+
+export default Header;
