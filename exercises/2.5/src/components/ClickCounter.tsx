e9905780b827706a682da2ae9f8893a9c537f7b5
hugodandois <hugo.dandois@student.vinci.be>
2024-10-21 15:17:34 +0200

    ex2.5-events

--- exercises/2.5/src/components/ClickCounter.tsx
+++ exercises/2.5/src/components/ClickCounter.tsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
+import './ClickCounter.css';
 
 interface ClickCounterProps {
     title: string;
     on10Click?: string;
+    onMouseOver?: string;
 }
 
-const ClickCounter = ({title,on10Click = "You are a master in the art of clicking !"}: ClickCounterProps) => {
+const ClickCounter = ({title,on10Click = "You are a master in the art of clicking !",onMouseOver='Click on me now !'}: ClickCounterProps) => {
     const [count, setCount] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
 
     const handleClick = () => {
         setCount(count + 1);
     };
 
+
     return (
         <div className="card">
             <h2>{title}</h2>
-            <button onClick={handleClick}>
+            
+            <button onClick={handleClick} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                 count is {count}
+                {isHovered ? <p>{onMouseOver}</p> : null}
             </button>
             {count >= 10 ? <p>{on10Click}</p> : null}
 
